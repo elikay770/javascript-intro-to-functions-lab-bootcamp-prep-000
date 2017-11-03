f3ca0a6e82d5e1e5d783ce7878392f70cf955b60
Elliot Kahan <ejkahan@gmail.com>
2017-11-03 09:49:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function logShout (string) {
 function logWhisper (string) {
   console.log ('hello')
 }
-function sayHiToGrandma (greeting) {
-if (greeting.toLowerCase() === greeting) {
-return `I can't hear you!`
-}}
-function sayHiToGrandma (greeting) {
-if (greeting.toUpperCase() === greeting) {
-return `YES INDEED!`
-}}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
