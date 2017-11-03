f957ea28c82f0cde8f50b5192094347b6f496d7b
Elliot Kahan <ejkahan@gmail.com>
2017-11-03 07:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,4 @@ function logShout (string) {
 function logWhisper (string) {
   console.log ('hello')
 }
-function sayHiToGrandma (string) {
-  logWhisper ('hello')
-return `I can't hear you!`
-}
+shout ('hello')
